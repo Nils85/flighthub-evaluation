7fba949a781bb8965ee9b348e90543725e37f567
Vince <vincent.boursier@gmail.com>
2019-08-30 19:48:41 -0400

    update the debug page

--- www/debug.php
+++ www/debug.php
@@ -15,32 +15,26 @@
 	<p>
 		<b>api/list_airports.php</b><br/>
 		<a href="api/list_airports.php">List all airports</a><br/>
-	</p>
-	<p>
+	</p>		
+	<form action="api/list_airports.php" method="get">
 		<b>api/list_airports.php?from=...</b><br/>
-		<form action="api/list_airports.php" method="get">
-			List connections between airports<br/>
-			<label for="txtbox4">From:</label><input type="text" id="txtbox4" name="from" value="YUL"/>
-			<input type="submit"/>
-		</form>
-	</p>
-	<p>
+		List connections between airports<br/>
+		<label for="txtbox4">From:</label><input type="text" id="txtbox4" name="from" value="YUL"/>
+		<input type="submit"/>
+	</form>
+	<form action="api/list_flights.php" method="get">
 		<b>api/list_flights.php?from=...&to=...</b><br/>
-		<form action="api/list_flights.php" method="get">
-			List available flights<br/>
-			<label for="txtbox1">From:</label><input type="text" id="txtbox1" name="from" value="YUL"/>
-			<label for="txtbox2">To:</label><input type="text" id="txtbox2" name="to" value="YVR"/>
-			<br/><input type="submit"/>
-		</form>
-	</p>
-	<p>
+		List available flights<br/>
+		<label for="txtbox1">From:</label><input type="text" id="txtbox1" name="from" value="YUL"/>
+		<label for="txtbox2">To:</label><input type="text" id="txtbox2" name="to" value="YVR"/>
+		<br/><input type="submit"/>
+	</form>
+	<form action="api/book_trip.php" method="post" enctype="multipart/form-data">
 		<b>api/book_trip.php</b><br/>
-		<form action="api/book_trip.php" method="post" enctype="multipart/form-data">
-			<label for="txtbox0">Book a trip (POST JSON)</label><br/>
-			<textarea id="txtbox0" name="flights">[{"flight":"AC301","date":"2019-09-29"},{"flight":"AC302","date":"2019-09-30"}]</textarea>
-			<br/><input type="submit"/>
-		</form>
-	</p>
+		<label for="txtbox0">Book a trip (POST JSON)</label><br/>
+		<textarea id="txtbox0" name="flights">[{"flight":"AC301","date":"2019-09-29"},{"flight":"AC302","date":"2019-09-30"}]</textarea>
+		<br/><input type="submit"/>
+	</form>
 	<p>
 		<b>api/list_trips.php</b><br/>
 		<a href="api/list_trips.php">List all trips</a><br/>
